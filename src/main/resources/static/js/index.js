34ce42ae99f25d41b30a3568d7013cf248f9491b
NicollLaguna <nicky7u7lagos@gmail.com>
2023-05-29 15:24:03 -0500

    casi terminado, faltan imagenes carrusel preguntas

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -50,3 +50,23 @@ nextItem.addEventListener('click', showNextItem);
 previousItem.addEventListener('click', showPreviousItem);
 document.addEventListener('keydown', keyPress);
 navItem.addEventListener('click', toggleNavigation);
+
+
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
+
+loginLink.addEventListener('click', () => {
+  wrapper.classList.remove('active');
+});
+
+btnPopup.addEventListener('click', () => {
+  wrapper.classList.add('active-popup');
+});
+
+
+iconClose.addEventListener('click', () => {
+  wrapper.classList.remove('active-popup');
+});
