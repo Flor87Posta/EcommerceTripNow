8a4002885293a8cc71f0e88d6807cdf0aaf756e3
NicollLaguna <nicky7u7lagos@gmail.com>
2023-05-31 13:54:59 -0500

    Co-authored-by: Mariana Cuba <MarianaCuba@users.noreply.github.com>

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,62 +1,26 @@
-const items = document.querySelectorAll('.slider-item');
-const itemCount = items.length;
-const nextItem = document.querySelector('.next');
-const previousItem = document.querySelector('.previous');
-const navItem = document.querySelector('a.toggle-nav');
-let count = 0;
-
-function showNextItem() {
-  items[count].classList.remove('active');
-
-  if(count < itemCount - 1) {
-    count++;
-  } else {
-    count = 0;
+const {createApp}= Vue;
+const app = createApp({
+  data(){
+    return{
+      email:'',
+      contrasena:'',
+    }
+  },
+  methods:{
+    Login(){
+      axios.post('/api/')
+    }
   }
-
-  items[count].classList.add('active');
-  console.log(count);
-}
-
-function showPreviousItem() {
-  items[count].classList.remove('active');
-
-  if(count > 0) {
-    count--;
-  } else {
-    count = itemCount - 1;
-  }
-
-  items[count].classList.add('active');
-  // Check if working...
-  console.log(count);
-}
-
-function toggleNavigation(){
-  this.nextElementSibling.classList.toggle('active');
-}
-
-function keyPress(e) {
-  e = e || window.event;
-  
-  if (e.keyCode == '37') {
-    showPreviousItem();
-  } else if (e.keyCode == '39') {
-    showNextItem();
-  }
-}
-
-nextItem.addEventListener('click', showNextItem);
-previousItem.addEventListener('click', showPreviousItem);
-document.addEventListener('keydown', keyPress);
-navItem.addEventListener('click', toggleNavigation);
-
+})
 
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
 const iconClose = document.querySelector('.icon-close');
+registerLink.addEventListener('click', () => {
+  wrapper.classList.add('active');
+});
 
 loginLink.addEventListener('click', () => {
   wrapper.classList.remove('active');
@@ -70,3 +34,27 @@ btnPopup.addEventListener('click', () => {
 iconClose.addEventListener('click', () => {
   wrapper.classList.remove('active-popup');
 });
+
+//boton
+const showOnPx = 100;
+const backToTopButton = document.querySelector(".back-to-top")
+
+const scrollContainer = () => {
+  return document.documentElement || document.body;
+};
+
+document.addEventListener("scroll", () => {
+  if (scrollContainer().scrollTop > showOnPx) {
+    backToTopButton.classList.remove("hidden")
+  } else {
+    backToTopButton.classList.add("hidden")
+  }
+})
+
+const goToTop = () => {
+  document.body.scrollIntoView({
+    behavior: "smooth",
+  });
+};
+
+backToTopButton.addEventListener("click", goToTop)
