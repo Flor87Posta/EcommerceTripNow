487b11f212e60fe621e15492a151d218433caa6e
FLORENCIA POSTACCHINI <florenciapostacchini@gmail.com>
2023-06-04 23:37:39 -0300

    manager

--- src/main/resources/static/manager/manager.js
+++ src/main/resources/static/manager/manager.js
@@ -152,15 +152,14 @@ const app = createApp({
                 stock: this.stock,
                 imagen1: this.imagen1,
                 pasaje: { id: this.pasaje },
-                destino: { id: this.destino }
+                destino: { id: this.destino },
+                hoteles: [] // Agrega la propiedad "hoteles" con un array vacío
             })
                 .then((response) => Swal.fire({
                     icon: 'success',
                     text: 'Paquete creado correctamente',
-
-                }
-
-                ))
+        
+                }))
                 .catch((error) => {
                     console.log(error.response.data)
                     Swal.fire({
@@ -231,33 +230,7 @@ const app = createApp({
     },
 
 
-    /*    loadData: obtiene el listado de clientes usando AJAX al back-end o servicio REST.
-    
-        Realiza una petición HTTP de tipo GET a la URL /clientes con la librería axios.
-    
-        Cuando la petición es respondida se ejecuta el método then
-    
-        El método then guarda en la data de Vue el listado de clientes que llega en el JSON así como el JSON completo.
-    
-        Vue se encarga de mostrar esos datos de manera automática, puedes volver a verificar el HTML para que veas las instrucciones de Vue que lo hacen.
-    
-    
-    
-        addClient: se ejecuta al pulsar el botón “add client” en la página web.
-    
-        Obtiene la data del formulario ya que los campos del mismo se encuentran asociados a la data de Vue.
-    
-        Si se han introducido los datos se ejecuta la función postClient
-    
-    
-    
-        postClient: obtiene los datos del nuevo cliente usando AJAX (peticiones asíncronas) al back-end o servicio REST.
-    
-        Realiza una petición HTTP de tipo POST a la URL /cients con la librería axios.
-    
-        Cuando la petición es respondida se ejecuta el método then
-    
-        El método then ejecuta la función loadData para que se recargue la información en la página.*/
+
 })
 
 app.mount('#app')
